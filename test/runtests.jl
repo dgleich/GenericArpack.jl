9597f00dddd507d0fc57a8145e954761e049f07d
David Gleich <dgleich@purdue.edu>
2021-05-14 09:16:25 -0400

    First arpack-based test -- missing files

--- test/runtests.jl
+++ test/runtests.jl
@@ -26,12 +26,26 @@ end
     @test_nowarn ArpackInJulia.dsconv(5, zeros(5), ones(5), 1e-8;
                     stats=ArpackInJulia.ArpackStats())
     stats = ArpackInJulia.ArpackStats()
-    @test ArpackInJulia.dsconv(5, ones(5), zeros(5), 1e-8; stats)==5
-    @test stats.tconv > 0  # we did record some time
+    @test ArpackInJulia.dsconv(15, ones(15), zeros(15), 1e-8; stats)==15
+    #@test stats.tconv > 0  # we did record some time
     t1 = stats.tconv
-    @test ArpackInJulia.dsconv(10, ones(10), zeros(10), 1e-8; stats)==10
-    @test stats.tconv > t1  # we did record some more time
+    @test ArpackInJulia.dsconv(25, ones(25), zeros(25), 1e-8; stats)==25
+    #@test stats.tconv > t1  # we did record some more time
 
     # TODO, add more relevant tests here.
+  end
+end
+
+if "arpackjll" in ARGS
+  include("arpackjll.jl") # get the helpful routines to call stuff in "arpackjll"
+  @testset "arpackjll" begin
+    @testset "dsconv" begin
+      soln = arpack_dsconv(10, ones(10), zeros(10), 1e-8)
+      @test ArpackInJulia.dsconv(10, ones(10), zeros(10), 1e-8)==soln
+
+      soln = arpack_dsconv(10, zeros(10), ones(10), 1e-8)
+      @test ArpackInJulia.dsconv(10, zeros(10), ones(10), 1e-8)==soln
+
+    end
   end
 end
