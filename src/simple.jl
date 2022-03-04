1ff408dbaa91ae733bb2a4e24054eacd88445073
David Gleich <dgleich@purdue.edu>
2022-03-04 15:46:22 -0500

    Draft versions of dsapps, dseigt

--- src/simple.jl
+++ src/simple.jl
@@ -917,3 +917,286 @@ c     %---------------%
 c
       end
 =#
+
+
+"""
+call dseigt
+( RNORM, N, H, LDH, EIG, BOUNDS, WORKL, IERR )
+
+Arguments
+RNORM   Double precision scalar.  (INPUT)
+     RNORM contains the residual norm corresponding to the current
+     symmetri tridiagonal matrix H.
+
+N       Integer.  (INPUT)
+     Size of the symmetri tridiagonal matrix H.
+
+H       Double precision N by 2 array.  (INPUT)
+     H contains the symmetri tridiagonal matrix with the 
+     subdiagonal in the first column starting at H(2,1) and the 
+     main diagonal in second column.
+
+LDH     Integer.  (INPUT)
+     Leading dimension of H exactly as declared in the calling 
+     program.
+
+EIG     Double precision array of length N.  (OUTPUT)
+     On output, EIG contains the N eigenvalues of H possibly 
+     unsorted.  The BOUNDS arrays are returned in the
+     same sorted order as EIG.
+
+BOUNDS  Double precision array of length N.  (OUTPUT)
+     On output, BOUNDS contains the error estimates corresponding
+     to the eigenvalues EIG.  This is equal to RNORM times the
+     last components of the eigenvectors corresponding to the
+     eigenvalues in EIG.
+
+WORKL   Double precision work array of length 3*N.  (WORKSPACE)
+     Private (replicated) array on each PE or array allocated on
+     the front end.
+
+Return value
+------------
+IERR    Integer.  (OUTPUT)
+     Error exit flag from dstqrb.
+"""
+function dseigt!(
+  rnorm::T,
+  n::Int,
+  H::AbstractMatrix{T},
+  ldh::Int,
+  eig::AbstractVecOrMat{T},
+  bounds::AbstractVecOrMat{T},
+  workl::AbstractVecOrMat{T}
+  ; 
+  state::Union{ArpackState{T},Nothing}=nothing, # not actually used
+  stats::Union{ArpackStats,Nothing}=nothing,
+  debug::Union{ArpackDebug,Nothing}=nothing,
+) where T 
+
+  @error("untested, do not use")
+  
+  @jl_arpack_check_length(eig, n)
+  @jl_arpack_check_length(bounds, n)
+  @jl_arpack_check_length(workl, 3n)
+  @jl_arpack_check_size(H,n,2)
+  @jl_arpack_check_size(H,ldh,2) # correct input size...
+
+  # c     | Initialize timing statistics  |
+  # c     | & message level for debugging |
+  t0 = @jl_arpack_time()
+  msglvl = @jl_arpack_debug(mseigt,0)
+
+  if msglvl > 0
+    _arpack_vout(debug, "_seigt: main diagonal of matrix H", @view(H[1:n,2]))
+    if n > 1
+      _arpack_vout(debug, "_seigt: sub diagonal of matrix H", @view(H[2:n,1]))
+    end 
+  end
+
+
+  copyto!(@view(eig[1:n]), @view(H[1:n,2]))
+  copyto!(@view(workl[1:n-1]), @view(H[2:n,1]))
+  ierr = dstqrb(n, @view(eig[1:n]), @view(workl[1:n-1]), @view(bounds[1:n]), @view(workl[1:n]))
+  if ierr != 0 
+    # failure, don't do anything and just return 
+  else
+    if msglvl > 1
+      _arpack_vout(debug, "_seigt: last row of the eigenvector matrix for H", 
+        @view(bounds[1:n]))
+    end
+    # c     | Finally determine the error bounds associated |
+    # c     | with the n Ritz values of H.                  |
+    for k=1:n
+      bounds[k] = rnorm*abs(bounds[k])
+    end
+  end
+
+  # note, arpack doesn't update this on a failure, but I think we should...
+  @jl_update_time(teigt, t0)
+  return ierr
+end 
+
+#=
+c-----------------------------------------------------------------------
+c\BeginDoc
+c
+c\Name: dseigt
+c
+c\Description: 
+c  Compute the eigenvalues of the current symmetric tridiagonal matrix
+c  and the corresponding error bounds given the current residual norm.
+c
+c\Usage:
+c  call dseigt
+c     ( RNORM, N, H, LDH, EIG, BOUNDS, WORKL, IERR )
+c
+c\Arguments
+c  RNORM   Double precision scalar.  (INPUT)
+c          RNORM contains the residual norm corresponding to the current
+c          symmetric tridiagonal matrix H.
+c
+c  N       Integer.  (INPUT)
+c          Size of the symmetric tridiagonal matrix H.
+c
+c  H       Double precision N by 2 array.  (INPUT)
+c          H contains the symmetric tridiagonal matrix with the 
+c          subdiagonal in the first column starting at H(2,1) and the 
+c          main diagonal in second column.
+c
+c  LDH     Integer.  (INPUT)
+c          Leading dimension of H exactly as declared in the calling 
+c          program.
+c
+c  EIG     Double precision array of length N.  (OUTPUT)
+c          On output, EIG contains the N eigenvalues of H possibly 
+c          unsorted.  The BOUNDS arrays are returned in the
+c          same sorted order as EIG.
+c
+c  BOUNDS  Double precision array of length N.  (OUTPUT)
+c          On output, BOUNDS contains the error estimates corresponding
+c          to the eigenvalues EIG.  This is equal to RNORM times the
+c          last components of the eigenvectors corresponding to the
+c          eigenvalues in EIG.
+c
+c  WORKL   Double precision work array of length 3*N.  (WORKSPACE)
+c          Private (replicated) array on each PE or array allocated on
+c          the front end.
+c
+c  IERR    Integer.  (OUTPUT)
+c          Error exit flag from dstqrb.
+c
+c\EndDoc
+c
+c-----------------------------------------------------------------------
+c
+c\BeginLib
+c
+c\Local variables:
+c     xxxxxx  real
+c
+c\Routines called:
+c     dstqrb  ARPACK routine that computes the eigenvalues and the
+c             last components of the eigenvectors of a symmetric
+c             and tridiagonal matrix.
+c     arscnd  ARPACK utility routine for timing.
+c     dvout   ARPACK utility routine that prints vectors.
+c     dcopy   Level 1 BLAS that copies one vector to another.
+c
+c\Author
+c     Danny Sorensen               Phuong Vu
+c     Richard Lehoucq              CRPC / Rice University 
+c     Dept. of Computational &     Houston, Texas 
+c     Applied Mathematics
+c     Rice University           
+c     Houston, Texas            
+c
+c\Revision history:
+c     xx/xx/92: Version ' 2.4'
+c
+c\SCCS Information: @(#) 
+c FILE: seigt.F   SID: 2.4   DATE OF SID: 8/27/96   RELEASE: 2
+c
+c\Remarks
+c     None
+c
+c\EndLib
+c
+c-----------------------------------------------------------------------
+c
+      subroutine dseigt 
+     &   ( rnorm, n, h, ldh, eig, bounds, workl, ierr )
+c
+c     %----------------------------------------------------%
+c     | Include files for debugging and timing information |
+c     %----------------------------------------------------%
+c
+      include   'debug.h'
+      include   'stat.h'
+c
+c     %------------------%
+c     | Scalar Arguments |
+c     %------------------%
+c
+      integer    ierr, ldh, n
+      Double precision
+     &           rnorm
+c
+c     %-----------------%
+c     | Array Arguments |
+c     %-----------------%
+c
+      Double precision
+     &           eig(n), bounds(n), h(ldh,2), workl(3*n)
+c
+c     %------------%
+c     | Parameters |
+c     %------------%
+c
+      Double precision
+     &           zero
+      parameter (zero = 0.0D+0)
+c
+c     %---------------%
+c     | Local Scalars |
+c     %---------------%
+c
+      integer    i, k, msglvl
+c
+c     %----------------------%
+c     | External Subroutines |
+c     %----------------------%
+c
+      external   dcopy, dstqrb, dvout, arscnd
+c
+c     %-----------------------%
+c     | Executable Statements |
+c     %-----------------------%
+c
+c     %-------------------------------%
+c     | Initialize timing statistics  |
+c     | & message level for debugging |
+c     %-------------------------------% 
+c
+      call arscnd (t0)
+      msglvl = mseigt
+c
+      if (msglvl .gt. 0) then
+         call dvout (logfil, n, h(1,2), ndigit,
+     &              '_seigt: main diagonal of matrix H')
+         if (n .gt. 1) then
+         call dvout (logfil, n-1, h(2,1), ndigit,
+     &              '_seigt: sub diagonal of matrix H')
+         end if
+      end if
+c
+      call dcopy  (n, h(1,2), 1, eig, 1)
+      call dcopy  (n-1, h(2,1), 1, workl, 1)
+      call dstqrb (n, eig, workl, bounds, workl(n+1), ierr)
+      if (ierr .ne. 0) go to 9000
+      if (msglvl .gt. 1) then
+         call dvout (logfil, n, bounds, ndigit,
+     &              '_seigt: last row of the eigenvector matrix for H')
+      end if
+c
+c     %-----------------------------------------------%
+c     | Finally determine the error bounds associated |
+c     | with the n Ritz values of H.                  |
+c     %-----------------------------------------------%
+c
+      do 30 k = 1, n
+         bounds(k) = rnorm*abs(bounds(k))
+   30 continue
+c 
+      call arscnd (t1)
+      tseigt = tseigt + (t1 - t0)
+c
+ 9000 continue
+      return
+c
+c     %---------------%
+c     | End of dseigt |
+c     %---------------%
+c
+      end
+=#
