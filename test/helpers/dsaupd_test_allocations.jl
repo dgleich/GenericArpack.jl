8e02a75a06aff6b2c175609d407e2c228e752659
David Gleich <dgleich@purdue.edu>
2022-03-30 09:50:48 -0400

    Add benchmarking code

--- test/helpers/dsaupd_test_allocations.jl
+++ test/helpers/dsaupd_test_allocations.jl
@@ -106,3 +106,39 @@ begin
     niter = 0 
   end
 end
+
+
+## profiling
+begin
+  op = ArpackInJulia.ArpackSimpleOp(Diagonal(1.0:10^3))
+  nev = 6
+  ido = Ref{Int}(0)
+  bmat = Val(:I)
+  n = size(op.A,1)
+  which = :LM
+  tol = 0.0 # just use the default
+  resid = zeros(n)
+  ncv = min(2nev, n-1)
+  V = zeros(n,ncv)
+  ldv = n
+  mode = 1 
+  iparam = zeros(Int,11)
+  iparam[1] = 1
+  #iparam[3] = 300 # max iteration
+  iparam[3] = 300 # 
+  iparam[4] = 1
+  iparam[7] = mode 
+  ipntr = zeros(Int,11)
+  workd = zeros(n,3)
+  lworkl = ncv*ncv + 8*ncv
+  workl = zeros(lworkl)
+
+  info_initv = 0
+
+  # Note that we cannot run two sequences at once and check them where we start a whole
+  # second arpack call because of the expected Arpack state. 
+  state = ArpackInJulia.ArpackState{Float64}()
+
+  niter = 0 
+  @profview eigrun(op, ido, bmat, n, which, nev, tol, resid, ncv, V, ldv, iparam, ipntr, workd, workl, lworkl, info_initv, state)
+end
